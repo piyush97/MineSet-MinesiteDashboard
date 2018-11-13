c42871f3758fcb959c7986f20d92b4592b8b61b5
Piyush Mehta <me@piyushmehta.com>
2018-11-14 01:59:51 +0530

    FETCHing ERROR
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/Typography/Typography.jsx
+++ src/views/Typography/Typography.jsx
@@ -13,7 +13,7 @@ import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import { Button } from "@material-ui/core";
-
+import axios from "axios";
 const style = {
   typo: {
     paddingLeft: "25%",
@@ -66,8 +66,63 @@ class TypographyPage extends React.Component {
   }
 
   handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
     event.preventDefault();
+    // request
+    //   .post(
+    //     "https://ufb8l0zd71.execute-api.us-east-1.amazonaws.com/default/edit_csv2")
+    //   .set("x-api-key", "8OlICBjgcw7SmSWwlcJOSrKvhVv9JuQ4kgDB2xY3")
+    //   .set("Content-Type", "application/json")
+    //   .send({
+    //     "location": "C",
+    //     "PM10": 13,
+    //     "PM25": 12,
+    //     "NO2": 172,
+    //     "O3": 23,
+    //     "CO": 256,
+    //     "SO2": 334,
+    //     "NH3": 412,
+    //     "Pb": 241,
+    //     "number": ["+919523419827", "+918762214723", "+919818782515"]
+    //   })
+    //   .end(res => {
+    //     console.log('res',res);
+    //   });
+
+    axios({
+      method: "post",
+      url:
+        "https://ufb8l0zd71.execute-api.us-east-1.amazonaws.com/default/edit_csv2",
+      data: {
+        "location": "C",
+        "PM10": "13",
+        "PM25": "12",
+        "NO2": "172",
+        "O3": "23",
+        "CO": "256",
+        "SO2": "334",
+        "NH3": "412",
+        "Pb": "241",
+        "number": ["+919523419827", "+918762214723", "+919818782515"]
+      },
+      config: {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Accept": "application/json",
+          "Access-Control-Allow-Headers":"origin",
+          "x-api-key": "8OlICBjgcw7SmSWwlcJOSrKvhVv9JuQ4kgDB2xY3"
+        }
+      }
+    }).then(function (response) {
+      //handle success
+      debugger
+      console.log(response);
+    })
+    .catch(function (error) {
+      //handle error
+      debugger
+      console.log(error);
+    });
+
   }
   render() {
     return (
