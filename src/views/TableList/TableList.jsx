017e8446dca770afb4d2b83255871c7ae4131330
Piyush Mehta <me@piyushmehta.com>
2018-11-13 17:01:34 +0530

    graph
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 // @material-ui/core components
 // core components
+import ChartistGraph from "react-chartist";
 import GridContainer from "components/Grid/GridContainer.jsx";
-
+import GridItem from "components/Grid/GridItem.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import { emailsSubscriptionChart } from "variables/charts.jsx";
+import AccessTime from "@material-ui/icons/AccessTime";
 class TableList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <GridContainer> </GridContainer>;
+    return (
+      <GridContainer> 
+        <GridItem xs={12} sm={6} md={6} lg={6}>
+          <Card chart>
+            <CardHeader color="warning">
+              <ChartistGraph
+                className="ct-chart"
+                data={emailsSubscriptionChart.data}
+                type="Bar"
+                options={emailsSubscriptionChart.options}
+                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                listener={emailsSubscriptionChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4>Air Quality</h4>
+              <p> Last Campaign Performance </p>
+            </CardBody>
+            <CardFooter chart>
+              <div>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    );
   }
 }
 
