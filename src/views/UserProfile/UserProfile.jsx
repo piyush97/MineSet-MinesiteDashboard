f4b564d16c7864a3c6d9edee4c331e380c6a9db3
Piyush Mehta <me@piyushmehta.com>
2018-11-13 13:44:57 +0530

    Accordian in data entry
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // import InputLabel from "@material-ui/core/InputLabel";
@@ -12,25 +13,22 @@ import Card from "components/Card/Card.jsx";
 // import CardAvatar from "components/Card/CardAvatar.jsx";
 // import CardBody from "components/Card/CardBody.jsx";
 // import CardFooter from "components/Card/CardFooter.jsx";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const styles = {
-  cardCategoryWhite: {
-    color: "rgba(255,255,255,.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0"
+const styles = theme => ({
+  root: {
+    width: '100%',
   },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none"
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
   }
-};
+});
+
 class UserProfile extends React.Component{
   constructor(props) {
     super(props);
@@ -40,11 +38,90 @@ class UserProfile extends React.Component{
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
         <GridContainer>
-          <GridItem xs={12} sm={12} md={8}>
-            <Card> </Card>
+          <GridItem xs={12} sm={12} md={12}>
+          <h1>Periodic Data Entry</h1>
+            <Card>
+              <div className={classes.root}>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Air Quality</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+              <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.heading}>Water Quality</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Ambient Noise Quality</Typography>
+                  </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                      </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+              <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.heading}>Meterological data</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </div>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={12}>
+          <h1>Annual Data Entry</h1>
+           <Card>
+              <div className={classes.root}>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Water Consumption</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+              <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.heading}>Raw Material Consumption</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </div>
+            </Card>
           </GridItem>
         </GridContainer>
       </div>
@@ -63,5 +140,7 @@ class UserProfile extends React.Component{
 //     </div>
 //   );
 // }
-
+UserProfile.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 export default withStyles(styles)(UserProfile);
