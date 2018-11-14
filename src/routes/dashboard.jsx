18f1d5b959d83d2ee64486ca09138290ed31392e
Piyush Mehta <me@piyushmehta.com>
2018-11-14 13:25:46 +0530

    PDF
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -3,7 +3,7 @@ import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 // import ContentPaste from "@material-ui/icons/ContentPaste";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
+// import BubbleChart from "@material-ui/icons/BubbleChart";
 // import LocationOn from "@material-ui/icons/LocationOn";
 // import Notifications from "@material-ui/icons/Notifications";
 // import Unarchive from "@material-ui/icons/Unarchive";
@@ -13,7 +13,7 @@ import UserProfile from "views/UserProfile/UserProfile.jsx";
 import TableList from "views/TableList/TableList.jsx";
 import Typography from "views/Typography/Typography.jsx";
 import Icons from "views/Icons/Icons.jsx";
-// import Maps from "views/Maps/Maps.jsx";
+import Maps from "views/Maps/Maps.jsx";
 // import NotificationsPage from "views/Notifications/Notifications.jsx";
 // import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.jsx";
 
@@ -50,16 +50,16 @@ const dashboardRoutes = [
     path: "/report",
     sidebarName: "Report Generation",
     navbarName: "Report Generation",
-    icon: BubbleChart,
+    icon: LibraryBooks,
     component: Icons
   },
-  // {
-  //   path: "/maps",
-  //   sidebarName: "Maps",
-  //   navbarName: "Map",
-  //   icon: LocationOn,
-  //   component: Maps
-  // },
+  {
+    path: "/certificate",
+    sidebarName: "Certification",
+    navbarName: "Certification",
+    icon: LibraryBooks,
+    component: Maps
+  },
   // {
   //   path: "/notifications",
   //   sidebarName: "Notifications",
--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,95 +1,48 @@
 import React from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import { Button } from "@material-ui/core";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
 
-const CustomSkinMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true,
-        styles: [
+class Maps extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.Certification = this.Certification.bind(this);
+  }
+  Certification() {
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    var docDefinition = {
+      pageOrientation: "landscape",
+      footer: {
+        columns: [
+          "Field Officer",
           {
-            featureType: "water",
-            stylers: [
-              { saturation: 43 },
-              { lightness: -11 },
-              { hue: "#0088ff" }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.fill",
-            stylers: [
-              { hue: "#ff0000" },
-              { saturation: -100 },
-              { lightness: 99 }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#808080" }, { lightness: 54 }]
-          },
-          {
-            featureType: "landscape.man_made",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ece2d9" }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ccdca1" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#767676" }]
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.stroke",
-            stylers: [{ color: "#ffffff" }]
-          },
-          { featureType: "poi", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "landscape.natural",
-            elementType: "geometry.fill",
-            stylers: [{ visibility: "on" }, { color: "#b8cb93" }]
-          },
-          { featureType: "poi.park", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.sports_complex",
-            stylers: [{ visibility: "on" }]
-          },
-          { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
-          {
-            featureType: "poi.business",
-            stylers: [{ visibility: "simplified" }]
+            text: "Governing Officer",
+            alignment: "right"
           }
         ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
-
-function Maps({ ...props }) {
-  return (
-    <CustomSkinMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
-  );
+      }
+    };
+    pdfMake.createPdf(docDefinition).download("Certificate.pdf");
+  }
+  render() {
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <center>
+              <Button style={{ color: "green", border: "5px solid" }} onClick={this.Certification}>
+                Get Certified
+              </Button>
+            </center>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
 
 export default Maps;
